523a37d73f38ed2cdb5261eaa7f5159ce29b7cfc
Nathan Rothenberger <nroth10@gmail.com>
2022-01-27 11:22:32 -0600

    clean up info markup

--- src/sections/section-info.tsx
+++ src/sections/section-info.tsx
@@ -12,53 +12,13 @@ const SectionInfo = () => (
   <Box bg="green.300">
     <Container>
       <SectionHeader>Info</SectionHeader>
-      <div id="whoiam" className="block">
-        {/* <p>Nike Jesuit Twilight Relays</p> */}
-        <Text textStyle="bodyLarge">
-          Jesuit High School and Nike are Proud to present the Pacific
-          Northwest’s premier high school track & field relay meet.
-        </Text>
-        {/* <p>
-          <strong>
-            THE 20TH&nbsp;ANNUAL NIKE/JESUIT TWILIGHT RELAYS WILL FEATURE:
-          </strong>
-        </p>
-        <ul>
-          <li>
-            AFTERNOON SESSION WITH MULTIPLE HEATS/FLIGHTS OF INDIVIDUAL EVENTS
-          </li>
-          <li>
-            TWILIGHT EVENING SESSION FULL OF EXCITING RELAY COMPETITION AND
-            ELITE 100 &amp; MILE RACES. THESE ELITE RACES&nbsp; HAVE YIELDED
-            SEVERAL TOP 10 U.S. PERFORMANCES.&nbsp;&nbsp;
-          </li>
-          <li>FIELD EVENTS WITH UP TO 48 ATHLETES PER EVENT</li>
-          <li>
-            GREAT COMPETITION WITH SOME OF THE BEST TEAMS REPRESENTED FROM
-            OREGON, WASHINGTON, IDAHO, MONTANA AND BEYOND.
-          </li>
-          <li>
-            PREMIER TRACK AND FIELD FACILITY THAT SHOWCASES A FAST, HIGH
-            PERFORMANCE TRACK SURFACE.
-          </li>
-          <li>UNIQUE AWARDS&nbsp;</li>
-          <li>PROFESSIONAL NIKE ATHLETE APPEARANCES</li>
-          <li>ELITE ATHLETE AND RELAY TEAM INTRODUCTIONS.</li>
-        </ul>
-        <p>
-          <strong>COME JOIN US!</strong>
-        </p>
-        <p>
-          <strong>
-            <a
-              href="https://www.athletic.net/TrackAndField/meet/440410/info"
-              target="_blank"
-            >
-              Click here for entry info
-            </a>
-          </strong>
-        </p> */}
-      </div>
+      {/* Text */}
+      <Text textStyle="bodyLarge" textAlign="center">
+        Jesuit High School and Nike are Proud to present the Pacific Northwest’s
+        premier high school track & field relay meet.
+      </Text>
+      {/* Photo CTA */}
+      <Box id="cta">
       <div id="cta">
         <div>
           <a href="https://www.athletic.net/TrackAndField/meet/440410/info">
