75de4fb1e2cb6722068fd4a91611cc2836a152f4
Nathan Rothenberger <nroth10@gmail.com>
2022-01-27 11:16:26 -0600

    add type style debug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,4 @@
-import {
-  Box,
-  Flex,
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-  Link
-} from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
-
+import { Box, Text, Container, VStack } from "@chakra-ui/react";
 import { SectionInfo, SectionSchedule } from "../sections";
 import { Banner } from "../components/Banner";
 import DesktopNav from "../components/DesktopNav";
@@ -19,6 +7,33 @@ const Index = () => (
   <Box>
     <Banner />
     <DesktopNav />
+    <Box bg="green.300">
+      <Container>
+        <VStack spacing="2" align="left">
+          <Text textStyle="sectionHeader" lineHeight="1">
+            sectionHeader
+          </Text>
+          <Text textStyle="scheduleTime" lineHeight="1">
+            scheduleTime
+          </Text>
+          <Text textStyle="scheduleTitle" lineHeight="1">
+            scheduleTitle
+          </Text>
+          <Text textStyle="bodyLarge" lineHeight="1">
+            bodyLarge
+          </Text>
+          <Text textStyle="navLink" lineHeight="1">
+            navLink
+          </Text>
+          <Text textStyle="photoTitle" lineHeight="1">
+            photoTitle
+          </Text>
+          <Text textStyle="scheduleType" lineHeight="1">
+            scheduleType
+          </Text>
+        </VStack>
+      </Container>
+    </Box>
     <SectionInfo />
     <SectionSchedule />
     {/* Bumper */}
