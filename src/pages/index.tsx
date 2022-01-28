d2f373f100e5238cc6e11f07251a0f7ddd0f10d5
Nathan Rothenberger <nroth10@gmail.com>
2022-01-28 10:26:13 -0600

    move section info into the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,40 @@
-import { Box, Text, Container, VStack } from "@chakra-ui/react";
-import { SectionInfo, SectionSchedule } from "../sections";
+import { Box, Text, Container, VStack, SimpleGrid } from "@chakra-ui/react";
+import { SectionSchedule } from "../sections";
 import { Banner } from "../components/Banner";
 import DesktopNav from "../components/DesktopNav";
 import TextStyles from "../components/TextStyles";
+import SectionHeader from "../components/SectionHeader";
+import ImageBlockCTA from "../components/ImageBlockCTA";
+import { imageBlocks } from "../lib/imageBlocks";
 
 const Index = () => (
   <Box>
     <Banner />
     <DesktopNav />
     {/* <TextStyles /> */}
-    <SectionInfo />
+    {/* Info */}
+    <Box>
+      <Container>
+        <SectionHeader>Info</SectionHeader>
+        {/* Text */}
+        <Text textStyle="bodyLarge" textAlign="center">
+          Jesuit High School and Nike are Proud to present the Pacific
+          Northwest’s premier high school track & field relay meet.
+        </Text>
+        {/* Blocks */}
+        <SimpleGrid display="flex" flexWrap="wrap" columnGap="0">
+          {imageBlocks.map(({ url, imageSrc, title }, i) => (
+            <ImageBlockCTA
+              url={url}
+              imageSrc={imageSrc}
+              title={title}
+              key={i}
+            />
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Box>
+    {/* Schedule */}
     <SectionSchedule />
     {/* Bumper */}
     <Box py="8" />
--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -1,2 +1,1 @@
-export * from "./section-info";
 export * from "./section-schedule";
