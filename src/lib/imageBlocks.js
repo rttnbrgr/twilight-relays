cfc6b61948c4a300007d07475c847a6ba4fbe429
Nathan Rothenberger <nroth10@gmail.com>
2022-04-09 16:43:38 -0500

    replace remaining image block images

--- src/lib/imageBlocks.js
+++ src/lib/imageBlocks.js
@@ -1,23 +1,18 @@
 export const imageBlocks = [
   {
     url: "https://www.athletic.net/TrackAndField/meet/440410/info",
-    imageSrc: "https://twilight-relays.surge.sh/assets/TR18_WebSquares-1.jpg",
+    imageSrc: "https://res.cloudinary.com/rttnbrgr/image/upload/v1649538136/Twilight%20Relays/TR18_WebSquares-1.jpg",
     title: "Info",
     height: '352',
     width: '352'
   },
   {
     url: "https://www.athletic.net/TrackAndField/meet/440410/info/133218",
-    imageSrc: "https://twilight-relays.surge.sh/assets/TR18_WebSquares-2.jpg",
+    imageSrc: "https://res.cloudinary.com/rttnbrgr/image/upload/v1649538136/Twilight%20Relays/TR18_WebSquares-2.jpg",
     title: "Records",
     height: '352',
     width: '352'
   },
-  // {
-  //   url: "https://live.athletictiming.net/meets/2446",
-  //   imageSrc: "https://twilight-relays.surge.sh/assets/TR18_WebSquares-2.jpg",
-  //   title: "Results"
-  // },
   {
     url: "https://twilightrelays.runnerspace.com/eprofile.php?event_id=96&amp;do=videos&amp;video_id=273064",
     // imageSrc: "https://twilight-relays.surge.sh/assets/TR18_WebSquares-3.jpg",
